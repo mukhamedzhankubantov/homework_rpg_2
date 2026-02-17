be99ef98f1ea17c2b374b9cc9ac900eda956074b
Kubantov Muhamedzhan <mukhamedzhan.kubantov@narxoz.kz>
2026-02-17 16:51:30 +0500

    feat: I added these concrete builders BasicEnamyBuilder and BossEnamyBuilder

--- src/com/narxoz/rpg/builder/BasicEnemyBuilder.java
+++ src/com/narxoz/rpg/builder/BasicEnemyBuilder.java
@@ -1,4 +1,91 @@
 package com.narxoz.rpg.builder;
 
-public class BasicEnemyBuilder {
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.enemy.Enemy;
+import com.narxoz.rpg.enemy.Goblin;
+import com.narxoz.rpg.loot.LootTable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BasicEnemyBuilder implements EnemyBuilder{
+
+    private String name;
+    private int health;
+    private int damage;
+    private int defense;
+    private int speed;
+    private String element;
+    private String aiBehavior;
+    private List<Ability> abilities = new ArrayList<>();
+    private LootTable lootTable;
+
+    @Override
+    public EnemyBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setHealth(int health) {
+        this.health = health;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setDamage(int damage) {
+        this.damage = damage;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setDefense(int defense) {
+        this.defense = defense;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setSpeed(int speed) {
+        this.speed = speed;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setElement(String element) {
+        this.element = element;
+        return this;
+    }
+
+
+    public EnemyBuilder setAbilities(List<Ability> abilities) {
+        this.abilities = abilities;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder addAbility(Ability ability) {
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder addPhase(int phaseNumber, int healthThreshold) {
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setAI(String aiBehavior) {
+        this.aiBehavior = aiBehavior;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setLootTable(LootTable lootTable) {
+        this.lootTable = lootTable;
+        return this;
+    }
+
+    @Override
+    public Enemy build() {
+        return new Goblin(name);
+    }
 }
---
+++ src/com/narxoz/rpg/builder/BossEnemyBuilder.java
@@ -0,0 +1,118 @@
+package com.narxoz.rpg.builder;
+
+import com.narxoz.rpg.combat.Ability;
+import com.narxoz.rpg.enemy.DragonBoss;
+import com.narxoz.rpg.enemy.Enemy;
+import com.narxoz.rpg.loot.LootTable;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BossEnemyBuilder implements EnemyBuilder{
+    private String name;
+    private int health;
+    private int damage;
+    private int defense;
+    private int speed;
+
+    private String element;
+    private String aiBehavior;
+
+    private List<Ability> abilities = new ArrayList<>();
+    private LootTable lootTable;
+
+    private Map<Integer, Integer> phases = new HashMap<>();
+
+    @Override
+    public EnemyBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+    @Override
+    public EnemyBuilder setHealth(int health) {
+        this.health = health;
+        return this;
+    }
+    @Override
+    public EnemyBuilder setDamage(int damage) {
+        this.damage = damage;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setDefense(int defense) {
+        this.defense = defense;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setSpeed(int speed) {
+        this.speed = speed;
+        return this;
+    }
+    @Override
+    public EnemyBuilder setElement(String element) {
+        this.element = element;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setAI(String aiBehavior) {
+        this.aiBehavior = aiBehavior;
+        return this;
+    }
+    @Override
+    public EnemyBuilder setAbilities(List<Ability> abilities) {
+        this.abilities = (abilities == null) ? new ArrayList<>() : abilities;
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder addAbility(Ability ability) {
+        if (ability != null) {
+            this.abilities.add(ability);
+        }
+        return this;
+    }
+    @Override
+    public EnemyBuilder addPhase(int phaseNumber, int healthThreshold) {
+        if (phaseNumber <= 0 || healthThreshold <= 0) return this;
+        phases.put(phaseNumber, healthThreshold);
+        return this;
+    }
+
+    @Override
+    public EnemyBuilder setLootTable(LootTable lootTable) {
+        this.lootTable = lootTable;
+        return this;
+    }
+
+    @Override
+    public Enemy build() {
+
+        if (name == null || name.isBlank()) {
+            throw new IllegalStateException("Name is required");
+        }
+        if (health <= 0) {
+            throw new IllegalStateException("Health must be positive");
+        }
+            int p1 = phases.getOrDefault(1,health);
+        int p2 = phases.getOrDefault(2,Math.max(1, health/2));
+        int p3 = phases.getOrDefault(3,Math.max(1,health/4));
+        boolean canFly =true;
+        boolean hasBreatAttack =true;
+        int wingspan =20;
+
+        return new DragonBoss(
+        name,health,damage,defense,speed,
+                element,
+                abilities,
+                p1,p2,p3,
+                lootTable,
+                aiBehavior,
+                canFly,hasBreatAttack,wingspan
+        );
+    }
+}
